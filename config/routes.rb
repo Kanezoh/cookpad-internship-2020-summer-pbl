b64e2f28cfe2fc7e92174eeb88e3072c59e6e9c3
Kanezoh <asfaltmanta114@gmail.com>
2020-08-27 09:14:25 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      get 'recipes', to: 'recipes#index'
+      get 'recipes/:id', to: 'recipes#show'
+    end
+  end
   root to: 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
