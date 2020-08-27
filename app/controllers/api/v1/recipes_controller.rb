a7145a0a96dc32781d6c5a745b3020035be0b9e3
Kanezoh <asfaltmanta114@gmail.com>
2020-08-27 22:30:21 +0900

    検索機能の改良

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -19,17 +19,23 @@ class Api::V1::RecipesController < ApplicationController
 
   def search
     search_word_str = params[:search_word]
-    search_words = search_word_str.split(' ').split('　').flatten
+    search_words = search_word_str.split(/ |　/).flatten
     search_words.map!{|word| to_hiragana(word)}
     filtered_recipes = []
     search_words.each do |search_word|
       filtered_recipes << Recipe.all.select do |recipe|
         to_hiragana(recipe.title).include?(search_word) ||
+        to_hiragana(recipe.description).include?(search_word) ||
         recipe.ingredients.pluck(:name).map{|name| to_hiragana(name) }.any?{|name| name.include?(search_word)}
       end
     end
     filtered_recipes.flatten!
-    render :json => {"recipes": filtered_recipes}
+    search_results = filtered_recipes
+    if search_words.size > 1
+      search_results = filtered_recipes.subtract_once(filtered_recipes.uniq)
+    end
+    p search_results
+    render :json => {"recipes": search_results}
   end
 
   private
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
 end
+
+class Array
+  def subtract_once(values)
+    counts = values.inject(Hash.new(0)) { |h, v| h[v] += 1; h }
+    reject { |e| counts[e] -= 1 unless counts[e].zero? }
+  end
+end
