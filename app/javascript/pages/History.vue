33ac40003706935c0a770e161cbcd38f7574a178
Kanezoh <asfaltmanta114@gmail.com>
2020-08-28 09:48:10 +0900

    履歴ページの作成

--- app/javascript/pages/History.vue
+++ app/javascript/pages/History.vue
@@ -1,9 +1,32 @@
 <template>
-  <p>history</p>
+  <div>
+    <h3>履歴</h3>
+    <v-card v-for="recipe in recipes" class="recipe-card mb-4" height="250px">
+      <router-link :to="`/recipes/${recipe.id}`" style="text-decoration: none; color: black;">
+        <v-row class="ma-0">
+          <v-col cols="6" sm="3" class="pa-0">
+            <v-img :src="recipe.img_path" class="recipe-img" height="250" />
+          </v-col>
+          <v-col cols="6" sm="9">
+            <v-card-title style="text-decoration: underline;">{{ recipe.title }}</v-card-title>
+            <v-card-text>{{ recipe.description }}</v-card-text>
+          </v-col>
+        </v-row>
+      </router-link>
+    </v-card>
+  </div>
 </template>
 
 <script>
 export default {
   name: 'History',
+  date: function() {
+    return {
+      recipes: []
+    }
+  },
+  created: function() {
+    this.recipes = JSON.parse(localStorage.getItem('recipeInfos')).reverse()
+  }
 }
 </script>
